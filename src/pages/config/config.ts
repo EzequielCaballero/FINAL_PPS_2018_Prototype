9b7a68d11d2d7ed0a6027def4717d2fbd6d69b0c
EzequielCaballero <ezequiel.caballero33@gmail.com>
2018-07-02 19:34:17 -0300

    Mejora de estilo al seleccionar bandera

--- src/pages/config/config.ts
+++ src/pages/config/config.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 //PAGES
 import { HomePage } from '../home/home';
@@ -10,6 +10,12 @@ import { text } from '../../assets/data/language_package';
   templateUrl: 'config.html',
 })
 export class ConfigPage {
+  @ViewChild('flag_es') flag_es:ElementRef;
+  @ViewChild('flag_en') flag_en:ElementRef;
+  @ViewChild('flag_ru') flag_ru:ElementRef;
+  @ViewChild('flag_de') flag_de:ElementRef;
+  @ViewChild('flag_fr') flag_fr:ElementRef;
+  @ViewChild('flag_pt') flag_pt:ElementRef;
 
   //SPINNER
   mostrarSpinner:boolean = false;
@@ -39,12 +45,59 @@ export class ConfigPage {
 
   selectLanguage(selection:string){
     switch(selection){
-      case "es": this.language = text.language.es; break;
-      case "en": this.language = text.language.en; break;
-      case "ru": this.language = text.language.ru; break;
-      case "de": this.language = text.language.de; break;
-      case "fr": this.language = text.language.fr; break;
-      case "pt": this.language = text.language.pt; break;
+      case "es":  this.language = text.language.es;
+                  this.flag_es.nativeElement.className = 'flag flag_selected';
+                  this.flag_en.nativeElement.className = 'flag';
+                  this.flag_ru.nativeElement.className = 'flag';
+                  this.flag_de.nativeElement.className = 'flag';
+                  this.flag_fr.nativeElement.className = 'flag';
+                  this.flag_pt.nativeElement.className = 'flag';
+                  break;
+
+      case "en":  this.language = text.language.en;
+                  this.flag_es.nativeElement.className = 'flag';
+                  this.flag_en.nativeElement.className = 'flag flag_selected';
+                  this.flag_ru.nativeElement.className = 'flag';
+                  this.flag_de.nativeElement.className = 'flag';
+                  this.flag_fr.nativeElement.className = 'flag';
+                  this.flag_pt.nativeElement.className = 'flag';
+                  break;
+
+      case "ru":  this.language = text.language.ru;
+                  this.flag_es.nativeElement.className = 'flag';
+                  this.flag_en.nativeElement.className = 'flag';
+                  this.flag_ru.nativeElement.className = 'flag flag_selected';
+                  this.flag_de.nativeElement.className = 'flag';
+                  this.flag_fr.nativeElement.className = 'flag';
+                  this.flag_pt.nativeElement.className = 'flag';
+                  break;
+
+      case "de":  this.language = text.language.de;
+                  this.flag_es.nativeElement.className = 'flag';
+                  this.flag_en.nativeElement.className = 'flag';
+                  this.flag_ru.nativeElement.className = 'flag';
+                  this.flag_de.nativeElement.className = 'flag flag_selected';
+                  this.flag_fr.nativeElement.className = 'flag';
+                  this.flag_pt.nativeElement.className = 'flag';
+                  break;
+
+      case "fr":  this.language = text.language.fr;
+                  this.flag_es.nativeElement.className = 'flag';
+                  this.flag_en.nativeElement.className = 'flag';
+                  this.flag_ru.nativeElement.className = 'flag';
+                  this.flag_de.nativeElement.className = 'flag';
+                  this.flag_fr.nativeElement.className = 'flag flag_selected';
+                  this.flag_pt.nativeElement.className = 'flag';
+                  break;
+
+      case "pt":  this.language = text.language.pt;
+                  this.flag_es.nativeElement.className = 'flag';
+                  this.flag_en.nativeElement.className = 'flag';
+                  this.flag_ru.nativeElement.className = 'flag';
+                  this.flag_de.nativeElement.className = 'flag';
+                  this.flag_fr.nativeElement.className = 'flag';
+                  this.flag_pt.nativeElement.className = 'flag flag_selected';
+                  break;
     }
   }
 
