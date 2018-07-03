fa6ff1c48af6cf81e214a298c6469d3769d75555
EzequielCaballero <ezequiel.caballero33@gmail.com>
2018-07-02 22:26:02 -0300

    Mejora en método Geocoding. Definición de lenguaje a partir del country code de un país

--- src/pages/config/config.ts
+++ src/pages/config/config.ts
@@ -26,6 +26,8 @@ export class ConfigPage implements AfterViewInit {
   language:any;
   lat:number;
   lng:number;
+  formatted_address:any;
+  address_components:any;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -69,20 +71,20 @@ export class ConfigPage implements AfterViewInit {
                   this.flag_pt.nativeElement.className = 'flag';
                   break;
 
-      case "ru":  this.language = text.language.ru;
-                  this.flag_es.nativeElement.className = 'flag';
-                  this.flag_en.nativeElement.className = 'flag';
-                  this.flag_ru.nativeElement.className = 'flag flag_selected';
-                  this.flag_de.nativeElement.className = 'flag';
-                  this.flag_fr.nativeElement.className = 'flag';
-                  this.flag_pt.nativeElement.className = 'flag';
-                  break;
-
       case "de":  this.language = text.language.de;
                   this.flag_es.nativeElement.className = 'flag';
                   this.flag_en.nativeElement.className = 'flag';
                   this.flag_ru.nativeElement.className = 'flag';
                   this.flag_de.nativeElement.className = 'flag flag_selected';
+                  this.flag_fr.nativeElement.className = 'flag';
+                  this.flag_pt.nativeElement.className = 'flag';
+                  break;
+
+      case "ru":  this.language = text.language.ru;
+                  this.flag_es.nativeElement.className = 'flag';
+                  this.flag_en.nativeElement.className = 'flag';
+                  this.flag_ru.nativeElement.className = 'flag flag_selected';
+                  this.flag_de.nativeElement.className = 'flag';
                   this.flag_fr.nativeElement.className = 'flag';
                   this.flag_pt.nativeElement.className = 'flag';
                   break;
@@ -107,19 +109,141 @@ export class ConfigPage implements AfterViewInit {
     }
   }
 
+  // MAP USER ACTION
   selectLocation(event){
     this.mostrarSpinner = true;
-    this._geoCoding.obtenerDireccion(event.coords.lat, event.coords.lng)
+    this._geoCoding.obtenerDireccionDetalle(event.coords.lat, event.coords.lng)
       .then((data:any)=>{
         this.lat = event.coords.lat;
         this.lng = event.coords.lng;
-        console.log("Dirección: " + data);
+        if(data != "undefined"){
+          this.formatted_address = data.direccion;
+          this.address_components = data.detalle;
+          console.log("Dirección: " + this.formatted_address);
+          console.log("Detalle: " + JSON.stringify(this.address_components));
+          this.define_countryCode();
+        }
         this.mostrarSpinner = false;
       })
       .catch((error)=>{
         console.log("ERROR: al convertir coordenadas -> dirección: " + error);
       })
-
   }
+
+//DEFINE COUNTRY CODE
+define_countryCode(){
+  for(let component of this.address_components){
+    if(component.short_name !== null){
+      console.log("DETALLE: " + component.short_name);
+      this.define_languageByCountryCode(component.short_name);
+    }
+  }
+}
+
+//DEFINE LANGUAGE BY COUNTRY CODE
+define_languageByCountryCode(code:string){
+  switch(code){
+    //ES - Español
+    case 'AR':
+    case 'UY':
+    case 'CL':
+    case 'PY':
+    case 'BO':
+    case 'PE':
+    case 'EC':
+    case 'CO':
+    case 'VE':
+    case 'PA':
+    case 'CR':
+    case 'NI':
+    case 'SV':
+    case 'HN':
+    case 'GT':
+    case 'DO':
+    case 'PR':
+    case 'MX':
+    case 'ES':
+    case 'GQ':
+    this.selectLanguage('es'); break;
+    //EN - English
+    case 'GY':
+    case 'US':
+    case 'BZ':
+    case 'VI':
+    case 'JM':
+    case 'US':
+    case 'IE':
+    case 'GB':
+    case 'PG':
+    case 'AU':
+    case 'NZ':
+    case 'SD':
+    case 'SL':
+    case 'LR':
+    case 'GH':
+    case 'NG':
+    case 'SS':
+    case 'KE':
+    case 'UG':
+    case 'ZM':
+    case 'MW':
+    case 'ZW':
+    case 'BW':
+    case 'NA':
+    case 'ZA':
+    case 'LS':
+    case 'SZ':
+    this.selectLanguage('en'); break;
+    //DE - Deutsch
+    case 'DE':
+    case 'CH':
+    case 'AT':
+    this.selectLanguage('de'); break;
+    //RU - Pусский
+    case 'BY':
+    case 'UA':
+    case 'MD':
+    case 'RU':
+    case 'KZ':
+    case 'UZ':
+    case 'KG':
+    case 'TJ':
+    this.selectLanguage('ru'); break;
+    //FR - Français
+    case 'GF':
+    case 'HT':
+    case 'CA':
+    case 'FR':
+    case 'BE':
+    case 'DZ':
+    case 'TN':
+    case 'ML':
+    case 'NE':
+    case 'TD':
+    case 'SN':
+    case 'GN':
+    case 'CI':
+    case 'TG':
+    case 'BF':
+    case 'NE':
+    case 'CM':
+    case 'CF':
+    case 'DJ':
+    case 'CD':
+    case 'GA':
+    case 'MG':
+    this.selectLanguage('fr'); break;
+    //PT - Portugues
+    case 'BR':
+    case 'PT':
+    case 'GW':
+    case 'CG':
+    case 'AO':
+    case 'MZ':
+    this.selectLanguage('pt'); break;
+    // default:
+    // this.selectLanguage('es'); break;
+  }
+}
 
 }
